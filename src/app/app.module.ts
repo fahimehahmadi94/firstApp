2313314abc5d34f689b9ecf3056d1d4b77ffdae3
fahimehahmadi.fafa <fahimehahmadi94@gmail.com>
2022-08-01 18:24:26 +0430

    ngif and ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductAlertsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+ 
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
